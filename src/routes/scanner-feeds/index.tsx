0eeebad9d55f1addbaf1df9b0f2aa9e222c88e0c
Ian Duke <ian@koho.ca>
2021-11-06 11:51:35 -0400

    Use https in Broadcastify iframe

--- src/routes/scanner-feeds/index.tsx
+++ src/routes/scanner-feeds/index.tsx
@@ -21,10 +21,10 @@ const ScannerFeeds: FunctionalComponent = () => {
 
             <section>
                 <h2>Toronto Fire East Command</h2>
-                <iframe class={style.player} src={`http://api.broadcastify.com/embed/player/?key=61000615&feedId=20553&html5=1&stats=1&as=1&bg=${bg}&fg=${fg}`} frameBorder="0">Your browser does not support iFrames.</iframe>
+                <iframe class={style.player} src={`https://api.broadcastify.com/embed/player/?key=61000615&feedId=20553&html5=1&stats=1&as=1&bg=${bg}&fg=${fg}`} frameBorder="0">Your browser does not support iFrames.</iframe>
 
                 <h2>Toronto Fire South Command and Automated Dispatch</h2>
-                <iframe class={style.player} src={`http://api.broadcastify.com/embed/player/?key=61000615&feedId=3140&html5=1&stats=1&as=1&bg=${bg}&fg=${fg}`} frameBorder="0">Your browser does not support iFrames.</iframe>
+                <iframe class={style.player} src={`https://api.broadcastify.com/embed/player/?key=61000615&feedId=3140&html5=1&stats=1&as=1&bg=${bg}&fg=${fg}`} frameBorder="0">Your browser does not support iFrames.</iframe>
             </section>
         </div>
     );
