83cffaf229863fa1630dff4a94ebe8a0e07f6496
Ian Duke <ian@koho.ca>
2022-08-04 00:17:35 -0400

    Feature: Contentful support (#9)
    * Fetch and render Blog entries from Contentful
    
    * Update deploy.yml

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,10 +1,30 @@
-import { FunctionalComponent, h } from 'preact';
+import { Component, h } from 'preact';
+import { ContentfulClientApi, createClient } from 'contentful';
 import cx from '../../lib/cx';
 import style from './style.scss';
 
-const Home: FunctionalComponent = () => {
-    return (
-        <div class={style.home}>
+class Home extends Component<any, any> {
+    client: ContentfulClientApi;
+
+    constructor() {
+        super();
+        this.state = {posts: []}
+        this.client = createClient({
+            accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || 'fallback value',
+            space: process.env.CONTENTFUL_SPACE_ID || 'fallback value',
+        });
+    }
+
+    componentDidMount() {
+        this.client.getEntries({content_type: 'post'})
+            .then((response: any) => {
+                const sortedItems = response.items.sort((a: any, b: any) =>  b.fields.date.localeCompare(a.fields.date));
+                this.setState({posts: sortedItems});
+            });
+    };
+
+    render() {
+        return <div class={style.home}>
             <section class={cx(style.section, style.group)}>
                 <div class={cx(style.col, style.span_1_of_2)}>
                     <h1>WELCOME</h1>
@@ -16,9 +36,20 @@ const Home: FunctionalComponent = () => {
 
             <section>
                 <h2>Club News</h2>
-                <p><strong>November 2021</strong>: Our annual dinner will be held at the Canadiana Restaurant (5230 Dundas Street West, Toronto) on Saturday, November 20th, 2021. Cocktails will start at 6pm along and dinner will be served at 7pm. You have 3 choices for dinner: beef, chicken or fish. Price is $40 per person. You must show proof of vaccination along with valid ID the night of the dinner, if not already given to Jeremy. The restaurant requires this information. We ask that you pay the night of only, just in case things change. Please RSVP by November 16, 2021 to either <a href="mailto:macrye322@hotmail.com">Dave MacDonald</a> or <a href="mailto:jfmckittrick@hotmail.com">Frank McKittrick</a>.</p>
-                <p><strong>September 2021</strong>: On Tuesday September 14 the club took delivery of the new Support 7. Due to COVID-19 protocols, members attended an open-house style event at the Rouge Hill GO Station parking lot. Check out <a href="https://www.blogto.com/eat_drink/2021/09/toronto-just-got-two-new-food-trucks-its-not-what-youre-thinking/">this article from blogTO</a>!</p>
+                {this.state.posts.map((post: any) => {
+                    return <article key={post.sys.id}>
+                                <h3>{post.fields.title}</h3>
+                                <i>Published {new Date(post.fields.date).toDateString()}</i>
+                                <p>{post.fields.body}</p>
+                                {post.fields.link &&
+                                    <p>Related link: <a href={post.fields.link}>{post.fields.link}</a></p>
+                                }
+                                <br/>
+                                <br/>
+                            </article>
+                })}
             </section>
+            
 
             <section>
                 <h2>Meetings</h2>
@@ -30,7 +61,7 @@ const Home: FunctionalComponent = () => {
                 <p>The Trumpet is the voice of the Greater Toronto Multiple Alarm Association. Since 1976 it has been the foremost source on fire service information in the Toronto area. No other publication gives you more - apparatus deliveries, department news, a monthly synopsis of major multiple alarm incidents, as well as the latest club news. Most issues are rounded out with interesting photos. For the low price of $10, receive a PDF version of The Trumpet in your inbox every month of the year. Contact us today to sign up for your subscription.</p>
             </section>
         </div>
-    );
+    };
 };
 
 export default Home;
