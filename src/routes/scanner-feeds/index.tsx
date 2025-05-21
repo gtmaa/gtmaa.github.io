3abee7618740bf0c802159e3d86a43f9f5f9544b
Ian Duke <ian.c.duke@gmail.com>
2025-05-21 17:05:46 -0400

    Remove dead radio feed

--- src/routes/scanner-feeds/index.tsx
+++ src/routes/scanner-feeds/index.tsx
@@ -22,12 +22,9 @@ const ScannerFeeds: FunctionalComponent = () => {
             </Helmet>
             <h1>TORONTO FIRE SCANNER FEEDS</h1>
 
-            <p>These feeds provide a live stream of TFS radio traffic for South and East Commands.</p>
+            <p>This feed provides a live stream of TFS radio traffic for South Command and the Automated Dispatch channel.</p>
 
             <section>
-                <h2>Toronto Fire East Command</h2>
-                <iframe class={style.player} src={`https://api.broadcastify.com/embed/player/?key=61000615&feedId=20553&html5=1&stats=1&as=1&bg=${bg}&fg=${fg}`} frameBorder="0">Your browser does not support iFrames.</iframe>
-
                 <h2>Toronto Fire South Command and Automated Dispatch</h2>
                 <iframe class={style.player} src={`https://api.broadcastify.com/embed/player/?key=61000615&feedId=3140&html5=1&stats=1&as=1&bg=${bg}&fg=${fg}`} frameBorder="0">Your browser does not support iFrames.</iframe>
             </section>
